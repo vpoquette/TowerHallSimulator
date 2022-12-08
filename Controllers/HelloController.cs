b8e2b241b4f1c179ee1fbecd5d528c3ba6bb18eb
Veronica Poquette <vpoquette@css.edu>
2022-12-08 10:49:10 -0600

    re-created and tweaked Unit 4 MVC assignment

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TowerHallSimulator.Models;
+
+namespace TowerHallSimulator.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Minnesota()
+        {
+            return View();
+        }
+        public IActionResult Wisconsin()
+        {
+            return View();
+        }
+        public IActionResult Maryland()
+        {
+            return View();
+        }
+    }
+}
