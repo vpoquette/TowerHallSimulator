2014a36cd25a2ec6d7497da2be7ea8bb9c1f6874
Veronica Poquette <vpoquette@css.edu>
2022-12-13 11:40:38 -0600

    added if statement to create html based on c# method results; tidied up FourthFloorClasses dramatically

--- FourthFloorClasses.cs
+++ FourthFloorClasses.cs
@@ -5,18 +5,20 @@ using System.Threading.Tasks;
 
 namespace TowerHallSimulator
 {
-    public class Classroom { }
     public class FourthFloorClasses : IClassroomInformation
     {
-        public static async Task Main() // does need to be async Task
+        public String testMethod()
+        {
+            return "yo";
+        }
+        public void Main()
         {
             string[] analogies = { "classroom", "sports", "video game", "animal", "job", "technology", "religion", "nature", "weather" };
             string[] reactions = { "understand perfectly", "are more confused than ever", "are starting to understand", "are nodding along" };
             string[] specialActivities = { "quiz", "film", "in-class essay", "guest speaker", "group activity", "technology", "independent project work"};
 
-            Classroom class4414 = new Classroom();
-
-            class4414 = await ClassInSession("4414", reactions, analogies, specialActivities);
+            FourthFloorClasses class4414 = new FourthFloorClasses();
+            class4414.ClassInSession("4414", reactions, analogies, specialActivities);
             // uncomment once we know one works
             /*
             Classroom class4605 = await ClassInSession("4605", reactions, analogies, specialActivities);
@@ -28,63 +30,61 @@ namespace TowerHallSimulator
             Classroom class4141 = await ClassInSession("4141", reactions, analogies, specialActivities);
             */
         }
-        public static async Task<Classroom> ClassInSession(String classNum, String[] reactions, String[] analogies, String[] specialActivities)
+        public void ClassInSession(String classNum, String[] reactions, String[] analogies, String[] specialActivities)
         {
-            var lecture = new Classroom();
             Random activity = new Random();
             int randomActivity = activity.Next(1, 2);
             if (randomActivity == 1) {
-                lecture = await Lecture(reactions, analogies, classNum);
+                Lecture(reactions, analogies, classNum);
             }
             else if (randomActivity == 2) { 
-                lecture = await SpecialActivity(specialActivities);
+                SpecialActivity(specialActivities);
             }
             ClassOver();
-            return lecture;
         }
-        public static async Task<Classroom> Lecture(String[] reactions, String[] analogies, String classNum)
+        public async void Lecture(String[] reactions, String[] analogies, String classNum)
         {
             RandomAnalogy(analogies, classNum);
             await Task.Delay(3000); // students must consider the analogy before they react
             RandomReaction(reactions);
-            return new Classroom();
         }
 
-        public static async Task<Classroom> SpecialActivity(String[] specialActivities)
+        public String SpecialActivity(String[] specialActivities)
         {
             Random randomActivities = new Random();
             int randomActivity = randomActivities.Next(0, specialActivities.Length);
-            await Task.Delay(1000); // give user a second to walk to the door
-            Console.Write("You look through the door and see the students engaged in a " + specialActivities[randomActivity] + ".");
+            String whatYouSee = ("You look through the door and see the students engaged in a " + specialActivities[randomActivity] + ".");
             RandomAttendance();
-            return new Classroom();
+            return whatYouSee;
         }
 
-        public static async void RandomAttendance()
+        public String RandomAttendance()
         {
             Random random = new Random();
             int attendance = random.Next(1, 101); // generates number from 1 to 100
-            await Task.Delay(1000); // give user a second to walk to the door
-            Console.Write(attendance + "% of students are in attendance today.");
+            String classAttendance = (attendance + "% of students are in attendance today.");
+            return classAttendance;
         }
 
-        public static void RandomAnalogy(String[] analogies, String classNum)
+        public String RandomAnalogy(String[] analogies, String classNum)
         {
             Random randomAnalogies = new Random();
             int randomAnalogy = randomAnalogies.Next(0, analogies.Length);
-            Console.Write("In classroom #" + classNum + ", the Professor uses a " + analogies[randomAnalogy] + " analogy to explain the concept.");
+            String profAnalogy = ("In classroom #" + classNum + ", the Professor uses a " + analogies[randomAnalogy] + " analogy to explain the concept.");
+            return profAnalogy;
         }
 
-        public static void RandomReaction(String[] reactions)
+        public String RandomReaction(String[] reactions)
         {
             Random randomReaction = new Random();
             int randomReact = randomReaction.Next(0, reactions.Length);
-            Console.Write("The students " + reactions[randomReact]);
+            String studentReaction = ("The students " + reactions[randomReact]);
+            return studentReaction;
         }
 
-        public static void ClassOver()
+        public String ClassOver()
         {
-            Console.WriteLine("Class dismissed!");
+            return "Class dismissed!";
         }
     }
 }
--- classroomActivity.cs
+++ classroomActivity.cs
@@ -7,55 +7,52 @@ namespace TowerHallSimulator
 {
     interface IClassroomInformation // interface segregation who
     {
-        async static Task<Classroom> ClassInSession(String classNum, String[] reactions, String[] analogies, String[] specialActivities)
+        void ClassInSession(String classNum, String[] reactions, String[] analogies, String[] specialActivities)
         {
-            var lecture = new Classroom();
             Random activity = new Random();
             int randomActivity = activity.Next(1, 2);
             if (randomActivity == 1)
             {
-                lecture = await Lecture(reactions, analogies, classNum);
+                Lecture(reactions, analogies, classNum);
             }
             else if (randomActivity == 2)
             {
-                lecture = await SpecialActivity(specialActivities);
+                SpecialActivity(specialActivities);
             }
-            return lecture;
         }
-        async static Task<Classroom> Lecture(String[] reactions, String[] analogies, String classNum)
+        async void Lecture(String[] reactions, String[] analogies, String classNum)
         {
             RandomAnalogy(analogies, classNum);
             await Task.Delay(3000); // students must consider the analogy before they react
             RandomReaction(reactions);
-            return new Classroom();
         }
-        async static Task<Classroom> SpecialActivity(String[] specialActivities)
+        string SpecialActivity(String[] specialActivities)
         {
             Random randomActivities = new Random();
             int randomActivity = randomActivities.Next(0, specialActivities.Length);
-            await Task.Delay(1000); // give user a second to walk to the door
-            Console.Write("You look through the door and see the students engaged in a " + specialActivities[randomActivity] + ".");
-            RandomAttendance();
-            return new Classroom();
+            String whatYouSee = ("You look through the door and see the students engaged in a " + specialActivities[randomActivity] + ".");
+            return whatYouSee;
         }
-        async static void RandomAttendance()
+        string RandomAttendance()
         {
             Random random = new Random();
             int attendance = random.Next(1, 101); // generates number from 1 to 100
-            await Task.Delay(1000); // give user a second to walk to the door
-            Console.Write(attendance + "% of students are in attendance today.");
+            String classAttendance = (attendance + "% of students are in attendance today.");
+            return classAttendance;
         }
-        static void RandomAnalogy(String[] analogies, String classNum)
+        string RandomAnalogy(String[] analogies, String classNum)
         {
             Random randomAnalogies = new Random();
             int randomAnalogy = randomAnalogies.Next(0, analogies.Length);
-            Console.Write("In classroom #" + classNum + ", the Professor uses a " + analogies[randomAnalogy] + " analogy to explain the concept.");
+            String profAnalogy = ("In classroom #" + classNum + ", the Professor uses a " + analogies[randomAnalogy] + " analogy to explain the concept.");
+            return profAnalogy;
         }
-        static void RandomReaction(String[] reactions)
+        string RandomReaction(String[] reactions)
         {
             Random randomReaction = new Random();
             int randomReact = randomReaction.Next(0, reactions.Length);
-            Console.Write("The students " + reactions[randomReact]);
+            String studentReaction = ("The students " + reactions[randomReact]);
+            return studentReaction;
         }
     }
 }
